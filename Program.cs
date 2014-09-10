f6c25d724f1f49654e69479864d0437cf3444046
Vitaliy Tartunov <Vitaliy.Tartunov@GMail.com>
2014-09-10 23:32:40 +0700

    Added main data

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Pinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new TrayWorker();
+
+            Application.Run();
+        }
+    }
+}
---
+++ TrayWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pinger
+{
+    public class TrayWorker
+    {
+        private NotifyIcon _notifyIcon;
+        private ContextMenuStrip _contextMenu;
+
+        public TrayWorker()
+        {
+            _notifyIcon = new NotifyIcon {Text = "Pinger", Icon = Properties.Resources.Main};
+
+            _contextMenu = new ContextMenuStrip();
+            _contextMenu.Items.Add("Good", Properties.Resources.Ok);
+            _contextMenu.Items.Add("Bad", Properties.Resources.Bad);
+            _contextMenu.Items.Add("-");
+            _contextMenu.Items.Add("Выход", Properties.Resources.Exit, OnQuit);
+
+            _notifyIcon.ContextMenuStrip = _contextMenu;
+            _notifyIcon.Visible = true;
+        }
+
+        private void OnQuit(object sender, EventArgs eventArgs)
+        {
+            _notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
